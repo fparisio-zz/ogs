84f373978513ddf699de3174a95763c130527ef1
fparisio <francesco.parisio@protonmail.com>
2017-03-07 14:52:22 +0100

    Creating new material Geosed
    Creating new files for Geosed in SolidModels & new namespace for the model

---
+++ MaterialLib/SolidModels/Geosed.cpp
@@ -0,0 +1,23 @@
+/**
+ * \copyright
+ * Copyright (c) 2012-2017, OpenGeoSys Community (http://www.opengeosys.org)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/project/license
+ *
+ */
+
+#include "Geosed.h"
+
+namespace MaterialLib
+{
+namespace Solids
+{
+namespace Geosed
+{
+template class SolidGeosed<2>;
+template class SolidGeosed<3>;
+
+}  // namespace Geosed
+}  // namespace Solids
+}  // namespace MaterialLib
