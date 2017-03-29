9adf451f0d0356ea3a9fa1258e670ef3be2136c3
Francesco Parisio <francesco.parisio@protonmail.com>
2017-03-29 11:14:25 +0200

    New brittleness measure

--- MaterialLib/SolidModels/Ehlers.h
+++ MaterialLib/SolidModels/Ehlers.h
@@ -254,6 +254,7 @@ public:
 private:
     void calculateLocalKappaD(
         double const t, ProcessLib::SpatialPosition const& x,
+        KelvinVector const& sigma,
         typename MechanicsBase<DisplacementDim>::MaterialStateVariables&
             material_state_variables);
 
