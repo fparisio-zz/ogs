71280c9ca033faca2ef8ed90aaad2b3ce09add98
Dmitri Naumov <dmitri.naumov@ufz.de>
2017-04-05 18:29:46 +0200

    [PL] Deformation: Add GradientMatrix type and computation.

--- ProcessLib/Deformation/LinearBMatrix.h
+++ ProcessLib/Deformation/LinearBMatrix.h
@@ -79,5 +79,67 @@ BMatrixType computeBMatrix(DNDX_Type const& dNdx,
     return B;
 }
 
+/// Fills a G-matrix based on given shape function dN/dx values.
+template <int DisplacementDim,
+          int NPOINTS,
+          typename N_Type,
+          typename DNDX_Type,
+          typename BMatrixType>
+void computeGMatrix(DNDX_Type const& dNdx,
+                    BMatrixType& g_matrix,
+                    const bool is_axially_symmetric,
+                    N_Type const& N,
+                    const double radius)
+{
+    static_assert(0 < DisplacementDim && DisplacementDim <= 3,
+                  "LinearGMatrix::computeGMatrix: DisplacementDim must be in "
+                  "range [1,3].");
+
+    g_matrix.setZero();
+
+    switch (DisplacementDim)
+    {
+        case 3:
+            // The gradient coordinates are organized in the following order:
+            // (1,1), (1,2), (1,3)
+            // (2,1), (2,2), (2,3)
+            // (3,1), (3,2), (3,3)
+            for (int d = 0; d < DisplacementDim; ++d){
+                for (int i = 0; i < NPOINTS; ++i)
+                {
+                    g_matrix(d + 0 * DisplacementDim, i + 0 * NPOINTS) =
+                        dNdx(d, i);
+                    g_matrix(d + 1 * DisplacementDim, i + 1 * NPOINTS) =
+                        dNdx(d, i);
+                    g_matrix(d + 2 * DisplacementDim, i + 2 * NPOINTS) =
+                        dNdx(d, i);
+                }
+            }
+            break;
+        case 2:
+            // The gradient coordinates are organized in the following order:
+            // (1,1), (1,2)
+            // (2,1), (2,2)
+            // (3,3)
+            for (int d = 0; d < DisplacementDim; ++d){
+                for (int i = 0; i < NPOINTS; ++i)
+                {
+                    g_matrix(d, i) = dNdx(d, i);
+                    g_matrix(d + DisplacementDim, i + NPOINTS) = dNdx(d, i);
+                }
+            }
+            if (is_axially_symmetric)
+            {
+                for (int i = 0; i < NPOINTS; ++i)
+                {
+                    g_matrix(4, i) = N[i] / radius;
+                }
+            }
+            break;
+        default:
+            break;
+    }
+}
+
 }  // namespace LinearBMatrix
 }  // namespace ProcessLib
