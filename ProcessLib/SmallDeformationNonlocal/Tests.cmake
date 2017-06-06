57a05c0edd6ee5119bb27ea9581e041b93333883
Francesco Parisio <francesco.parisio@protonmail.com>
2017-06-06 16:07:35 +0200

    Nooru_Mohamed Tests CMAKE

---
+++ ProcessLib/SmallDeformationNonlocal/Tests.cmake
@@ -0,0 +1,253 @@
+# Mechanics; Small deformations, linear (SDL)
+AddTest(
+    NAME Mechanics_SDL_square_1e0_displacementBC
+    PATH Mechanics/Linear
+    EXECUTABLE ogs
+    EXECUTABLE_ARGS square_1e0.prj
+    TESTER vtkdiff
+    REQUIREMENTS NOT OGS_USE_MPI
+    ABSTOL 1e-16 RELTOL 1e-16
+    DIFF_DATA
+    square_1e0_expected_pcs_0_ts_4_t_1.000000.vtu square_1e0_pcs_0_ts_4_t_1.000000.vtu displacement displacement
+)
+AddTest(
+    NAME Mechanics_SDL_square_1e2_tractionBC
+    PATH Mechanics/Linear
+    EXECUTABLE ogs
+    EXECUTABLE_ARGS square_1e2.prj
+    TESTER vtkdiff
+    REQUIREMENTS NOT OGS_USE_MPI
+    ABSTOL 1e-16 RELTOL 1e-16
+    DIFF_DATA
+    square_1e2_expected_pcs_0_ts_4_t_1.000000.vtu square_1e2_pcs_0_ts_4_t_1.000000.vtu displacement displacement
+)
+AddTest(
+    NAME LARGE_Mechanics_SDL_disc_with_hole
+    PATH Mechanics/Linear
+    EXECUTABLE ogs
+    EXECUTABLE_ARGS disc_with_hole.prj
+    TESTER vtkdiff
+    REQUIREMENTS NOT OGS_USE_MPI
+    ABSTOL 1e-16 RELTOL 1e-16
+    DIFF_DATA
+    disc_with_hole_expected_pcs_0_ts_4_t_1.000000.vtu disc_with_hole_pcs_0_ts_4_t_1.000000.vtu displacement displacement
+    VIS disc_with_hole_pcs_0_ts_4_t_1.000000.vtu
+)
+AddTest(
+    NAME LARGE_Mechanics_SDL_square_1e5_tractionBC
+    PATH Mechanics/Linear
+    EXECUTABLE ogs
+    EXECUTABLE_ARGS square_1e5.prj
+    TESTER vtkdiff
+    REQUIREMENTS NOT OGS_USE_MPI
+    ABSTOL 1e-16 RELTOL 1e-16
+    DIFF_DATA
+    square_1e5_expected_pcs_0_ts_4_t_1.000000.vtu square_1e5_pcs_0_ts_4_t_1.000000.vtu displacement displacement
+)
+AddTest(
+    NAME Mechanics_SDL_square_1e2_quad8_traction_topBC
+    PATH Mechanics/Linear
+    EXECUTABLE ogs
+    EXECUTABLE_ARGS square_1e2_quad8_traction_top.prj
+    WRAPPER time
+    TESTER vtkdiff
+    REQUIREMENTS NOT OGS_USE_MPI
+    ABSTOL 1e-14 RELTOL 1e-15
+    DIFF_DATA
+    expected_square_1e2_quad8_traction_topBC_pcs_0_ts_4_t_1.000000.vtu square_1e2_quad8_traction_topBC_pcs_0_ts_4_t_1.000000.vtu displacement displacement
+)
+# Mechanics; Small deformations, linear (SDL); Nodal forces
+AddTest(
+    NAME Mechanics_SDL_cube_1e0_nodal_forces
+    PATH Mechanics/Linear
+    EXECUTABLE ogs
+    EXECUTABLE_ARGS cube_1e0.prj
+    WRAPPER time
+    TESTER vtkdiff
+    REQUIREMENTS NOT OGS_USE_MPI
+    ABSTOL 1e-14 RELTOL 1e-15
+    DIFF_DATA
+    expected_cube_1e0_pcs_0_ts_4_t_1.000000.vtu cube_1e0_pcs_0_ts_4_t_1.000000.vtu displacement displacement
+    expected_cube_1e0_pcs_0_ts_4_t_1.000000.vtu cube_1e0_pcs_0_ts_4_t_1.000000.vtu NodalForces NodalForces
+)
+AddTest(
+    NAME Mechanics_SDL_cube_1e0_simple_shear_nodal_forces
+    PATH Mechanics/Linear
+    EXECUTABLE ogs
+    EXECUTABLE_ARGS cube_1e0_simple_shear.prj
+    WRAPPER time
+    TESTER vtkdiff
+    REQUIREMENTS NOT OGS_USE_MPI
+    ABSTOL 1e-14 RELTOL 1e-15
+    DIFF_DATA
+    expected_cube_1e0_simple_shear_pcs_0_ts_4_t_1.000000.vtu cube_1e0_simple_shear_pcs_0_ts_4_t_1.000000.vtu displacement displacement
+    expected_cube_1e0_simple_shear_pcs_0_ts_4_t_1.000000.vtu cube_1e0_simple_shear_pcs_0_ts_4_t_1.000000.vtu NodalForces NodalForces
+)
+
+
+# Mechanics; Small deformations, Burgers (SDB)
+AddTest(
+    NAME Mechanics_SDB_cube_1e0_tractionBC
+    PATH Mechanics/Burgers
+    EXECUTABLE ogs
+    EXECUTABLE_ARGS cube_1e0.prj
+    TESTER vtkdiff
+    REQUIREMENTS NOT OGS_USE_MPI
+    ABSTOL 1e-16 RELTOL 1e-16
+    DIFF_DATA
+    cube_1e0_expected_pcs_0_ts_1_t_0.000100.vtu cube_1e0_pcs_0_ts_1_t_0.000100.vtu displacement displacement
+    cube_1e0_expected_pcs_0_ts_101_t_1.000000.vtu cube_1e0_pcs_0_ts_101_t_1.000000.vtu displacement displacement
+)
+AddTest(
+    NAME LARGE_Mechanics_SDB_cube_1e3_tractionBC
+    PATH Mechanics/Burgers
+    EXECUTABLE ogs
+    EXECUTABLE_ARGS cube_1e3.prj
+    TESTER vtkdiff
+    REQUIREMENTS NOT OGS_USE_MPI
+    ABSTOL 1e-16 RELTOL 1e-16
+    DIFF_DATA
+    cube_1e3_expected_pcs_0_ts_1_t_0.000100.vtu cube_1e3_pcs_0_ts_1_t_0.000100.vtu displacement displacement
+    cube_1e3_expected_pcs_0_ts_101_t_1.000000.vtu cube_1e3_pcs_0_ts_101_t_1.000000.vtu displacement displacement
+)
+
+# Mechanics; Small deformations, Ehlers (SDE)
+AddTest(
+    NAME Mechanics_SDE_cube_1e0
+    PATH Mechanics/Ehlers
+    EXECUTABLE ogs
+    EXECUTABLE_ARGS cube_1e0.prj
+    TESTER vtkdiff
+    REQUIREMENTS NOT OGS_USE_MPI
+    ABSTOL 1e-15 RELTOL 1e-13
+    DIFF_DATA
+    expected_cube_1e0_pcs_0_ts_101_t_2.550000.vtu cube_1e0_pcs_0_ts_101_t_2.550000.vtu displacement displacement
+    expected_cube_1e0_pcs_0_ts_101_t_2.550000.vtu cube_1e0_pcs_0_ts_101_t_2.550000.vtu sigma_xx sigma_xx
+    expected_cube_1e0_pcs_0_ts_203_t_5.100000.vtu cube_1e0_pcs_0_ts_203_t_5.100000.vtu displacement displacement
+    expected_cube_1e0_pcs_0_ts_203_t_5.100000.vtu cube_1e0_pcs_0_ts_203_t_5.100000.vtu sigma_xx sigma_xx
+)
+AddTest(
+    NAME Mechanics_SDE_cube_1e1
+    PATH Mechanics/Ehlers
+    EXECUTABLE ogs
+    EXECUTABLE_ARGS cube_1e1.prj
+    TESTER vtkdiff
+    REQUIREMENTS NOT OGS_USE_MPI
+    ABSTOL 1e-15 RELTOL 1e-13
+    DIFF_DATA
+    expected_cube_1e1_pcs_0_ts_101_t_2.550000.vtu cube_1e1_pcs_0_ts_101_t_2.550000.vtu displacement displacement
+    expected_cube_1e1_pcs_0_ts_101_t_2.550000.vtu cube_1e1_pcs_0_ts_101_t_2.550000.vtu sigma_xx sigma_xx
+    expected_cube_1e1_pcs_0_ts_203_t_5.100000.vtu cube_1e1_pcs_0_ts_203_t_5.100000.vtu displacement displacement
+    expected_cube_1e1_pcs_0_ts_203_t_5.100000.vtu cube_1e1_pcs_0_ts_203_t_5.100000.vtu sigma_xx sigma_xx
+)
+AddTest(
+    NAME LARGE_Mechanics_SDE_cube_1e3
+    PATH Mechanics/Ehlers
+    EXECUTABLE ogs
+    EXECUTABLE_ARGS cube_1e3.prj
+    TESTER vtkdiff
+    REQUIREMENTS NOT OGS_USE_MPI
+    ABSTOL 3e-13 RELTOL 1e-16
+    DIFF_DATA
+    expected_cube_1e3_pcs_0_ts_101_t_2.550000.vtu cube_1e3_pcs_0_ts_101_t_2.550000.vtu displacement displacement
+    expected_cube_1e3_pcs_0_ts_101_t_2.550000.vtu cube_1e3_pcs_0_ts_101_t_2.550000.vtu sigma_xx sigma_xx
+    expected_cube_1e3_pcs_0_ts_203_t_5.100000.vtu cube_1e3_pcs_0_ts_203_t_5.100000.vtu displacement displacement
+    expected_cube_1e3_pcs_0_ts_203_t_5.100000.vtu cube_1e3_pcs_0_ts_203_t_5.100000.vtu sigma_xx sigma_xx
+)
+
+# Mechanics; Small deformations, Drucker-Prager (SDE-DP)
+AddTest(
+    NAME Mechanics_PlasticModel_SDE-DP_Ehlers_SpecialCase_DruckerPrager
+    PATH Mechanics/Ehlers
+    EXECUTABLE ogs
+    EXECUTABLE_ARGS cube_1e0_dp.prj
+    TESTER vtkdiff
+    REQUIREMENTS NOT OGS_USE_MPI
+    ABSTOL 5e-15 RELTOL 1e-13
+    DIFF_DATA
+    expected_cube_1e0_dp_pcs_0_ts_203_t_5.100000.vtu cube_1e0_dp_pcs_0_ts_203_t_5.100000.vtu displacement displacement
+    expected_cube_1e0_dp_pcs_0_ts_203_t_5.100000.vtu cube_1e0_dp_pcs_0_ts_203_t_5.100000.vtu sigma_xx sigma_xx
+)
+
+
+# SMALL DEFORMATION TEST -- AXIALLY SYMMETRIC
+AddTest(
+    NAME LARGE_SmallDeformation_ring_plane_strain_axi
+    PATH Mechanics/Linear
+    EXECUTABLE ogs
+    EXECUTABLE_ARGS ring_plane_strain.prj
+    TESTER vtkdiff
+    REQUIREMENTS NOT OGS_USE_MPI
+    ABSTOL 6e-4 RELTOL 1e-4
+    DIFF_DATA
+    ring_plane_strain_1e4_solution.vtu ring_plane_strain_pcs_0_ts_1_t_1.000000.vtu u displacement
+    ring_plane_strain_1e4_solution.vtu ring_plane_strain_pcs_0_ts_1_t_1.000000.vtu sigma_rr sigma_rr
+    ring_plane_strain_1e4_solution.vtu ring_plane_strain_pcs_0_ts_1_t_1.000000.vtu sigma_ff sigma_ff
+)
+
+# Mechanics; Small deformations, Ehlers-damage Uniaxial Tension (SDED)
+AddTest(
+    NAME Mechanics_PlasticModel_SDED_Ehlers_Damage_UniaxialTension
+    PATH Mechanics/EhlersDamage/UniaxialTension
+    EXECUTABLE ogs
+    EXECUTABLE_ARGS data.prj
+    TESTER vtkdiff
+    REQUIREMENTS NOT OGS_USE_MPI
+    ABSTOL 5e-15 RELTOL 1e-13
+    DIFF_DATA
+    uc_01_pcs_0_ts_3276_t_3.276000.vtu uc_01_pcs_0_ts_3276_t_3.276000.vtu displacement displacement
+    uc_01_pcs_0_ts_3276_t_3.276000.vtu uc_01_pcs_0_ts_3276_t_3.276000.vtu sigma_yy sigma_yy
+    uc_01_pcs_0_ts_5000_t_5.000000.vtu uc_01_pcs_0_ts_5000_t_5.000000.vtu displacement displacement
+    uc_01_pcs_0_ts_5000_t_5.000000.vtu uc_01_pcs_0_ts_5000_t_5.000000.vtu sigma_yy sigma_yy
+)
+
+# Mechanics; Small deformations, Ehlers-damage Uniaxial Compression (SDED)
+AddTest(
+    NAME Mechanics_PlasticModel_SDED_Ehlers_Damage_UniaxialCompression
+    PATH Mechanics/EhlersDamage/UniaxialCompression
+    EXECUTABLE ogs
+    EXECUTABLE_ARGS data.prj
+    TESTER vtkdiff
+    REQUIREMENTS NOT OGS_USE_MPI
+    ABSTOL 5e-15 RELTOL 1e-13
+    DIFF_DATA
+    uc_01_pcs_0_ts_3543_t_3.543000.vtu uc_01_pcs_0_ts_3543_t_3.543000.vtu displacement displacement
+    uc_01_pcs_0_ts_3543_t_3.543000.vtu uc_01_pcs_0_ts_3543_t_3.543000.vtu sigma_yy sigma_yy
+    uc_01_pcs_0_ts_5000_t_5.000000.vtu uc_01_pcs_0_ts_5000_t_5.000000.vtu displacement displacement
+    uc_01_pcs_0_ts_5000_t_5.000000.vtu uc_01_pcs_0_ts_5000_t_5.000000.vtu sigma_yy sigma_yy
+)
+
+# Mechanics; Small deformations, Ehlers-damage Triaxial Compression (SDED)
+AddTest(
+    NAME Mechanics_PlasticModel_SDED_Ehlers_Damage_TriaxialCompression
+    PATH Mechanics/EhlersDamage/TriaxialCompression
+    EXECUTABLE ogs
+    EXECUTABLE_ARGS data.prj
+    TESTER vtkdiff
+    REQUIREMENTS NOT OGS_USE_MPI
+    ABSTOL 5e-15 RELTOL 1e-13
+    DIFF_DATA
+    uc_01_pcs_0_ts_2823_t_2.823000.vtu uc_01_pcs_0_ts_2823_t_2.823000.vtu displacement displacement
+    uc_01_pcs_0_ts_2823_t_2.823000.vtu uc_01_pcs_0_ts_2823_t_2.823000.vtu sigma_yy sigma_yy
+    uc_01_pcs_0_ts_5000_t_5.000000.vtu uc_01_pcs_0_ts_5000_t_5.000000.vtu displacement displacement
+    uc_01_pcs_0_ts_5000_t_5.000000.vtu uc_01_pcs_0_ts_5000_t_5.000000.vtu sigma_yy sigma_yy
+)
+
+# Mechanics; Small deformations Non-Local, Ehlers-damage Nooru-Mohamed (NMED)
+AddTest(
+    NAME Mechanics_PlasticDamageNonlocalModel_NMED_Ehlers_Damage_NooruMohamed
+    PATH Mechanics/EhlersDamage/Nooru_Mohamed
+    EXECUTABLE ogs
+    EXECUTABLE_ARGS nooru_m.prj
+    TESTER vtkdiff
+    REQUIREMENTS NOT OGS_USE_MPI
+    ABSTOL 5e-15 RELTOL 1e-13
+    DIFF_DATA
+    b_f__pcs_0_ts_30_t_0.030000.vtu b_f__pcs_0_ts_30_t_0.030000.vtu displacement damage
+    b_f__pcs_0_ts_30_t_0.030000.vtu b_f__pcs_0_ts_30_t_0.030000.vtu displacement syy
+    b_f__pcs_0_ts_40_t_0.040000.vtu b_f__pcs_0_ts_40_t_0.040000.vtu displacement damage
+    b_f__pcs_0_ts_40_t_0.040000.vtu b_f__pcs_0_ts_40_t_0.040000.vtu displacement syy
+    b_f__pcs_0_ts_50_t_0.050000.vtu b_f__pcs_0_ts_50_t_0.050000.vtu displacement damage
+    b_f__pcs_0_ts_50_t_0.050000.vtu b_f__pcs_0_ts_50_t_0.050000.vtu displacement syy
+)
+
