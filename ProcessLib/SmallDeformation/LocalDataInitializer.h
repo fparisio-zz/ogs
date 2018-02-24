a7f9cd0aec6fd00b5f5511988b2a1ce282bc7873
Dmitri Naumov <github@naumov.de>
2018-02-24 13:34:16 +0100

    [PL] SD: Add Gauss-Lobatto to LocalDataInitializer

--- ProcessLib/SmallDeformation/LocalDataInitializer.h
+++ ProcessLib/SmallDeformation/LocalDataInitializer.h
@@ -19,6 +19,7 @@
 #include "MeshLib/Elements/Elements.h"
 #include "NumLib/DOF/LocalToGlobalIndexMap.h"
 #include "NumLib/Fem/Integration/GaussLegendreIntegrationPolicy.h"
+#include "NumLib/Fem/Integration/GaussLobattoIntegrationPolicy.h"
 
 #ifndef OGS_MAX_ELEMENT_DIM
 static_assert(false, "The macro OGS_MAX_ELEMENT_DIM is undefined.");
