43036f333e5addc0f7a9f0fa831ae62d456b35e8
Dmitri Naumov <dmitri.naumov@ufz.de>
2018-03-02 21:15:58 +0100

    [PL] SDN: Comment repeat pressure convergence.

--- ProcessLib/SmallDeformationNonlocal/SmallDeformationNonlocalProcess.cpp
+++ ProcessLib/SmallDeformationNonlocal/SmallDeformationNonlocalProcess.cpp
@@ -379,7 +379,7 @@ SmallDeformationNonlocalProcess<DisplacementDim>::postIterationConcreteProcess(
     // TODO (parisio) try this to enforce pressure convergence.
     if (_process_data.pressure_error > 1e-4)
     {
-        return NumLib::IterationResult::REPEAT_ITERATION;
+        // return NumLib::IterationResult::REPEAT_ITERATION;
     }
 
 
