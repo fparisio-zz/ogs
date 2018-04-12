4614bb577add9545fa76b5bc095e5d21fe976896
Francesco Parisio <francesco.parisio@ufz.de>
2018-04-12 12:11:38 +0200

    Create models and parameters

--- MaterialLib/SolidModels/CreateThermoPlasticBDT.h
+++ MaterialLib/SolidModels/CreateThermoPlasticBDT.h
@@ -9,27 +9,28 @@
 
 #pragma once
 
-#include "Ehlers.h"
 #include "ProcessLib/Utils/ProcessUtils.h"  // required for findParameter
+
+#include "ThermoPlasticBDT.h"
 
 namespace MaterialLib
 {
 namespace Solids
 {
-namespace Ehlers
+namespace ThermoPlasticBDT
 {
 inline NumLib::NewtonRaphsonSolverParameters
 createNewtonRaphsonSolverParameters(BaseLib::ConfigTree const& config)
 {
     DBUG("Create local nonlinear solver parameters.");
     auto const maximum_iterations =
-        //! \ogs_file_param{material__solid__constitutive_relation__Ehlers__nonlinear_solver__maximum_iterations}
+        //! \ogs_file_param{material__solid__constitutive_relation__ThermoPlasticBDT__nonlinear_solver__maximum_iterations}
         config.getConfigParameter<int>("maximum_iterations");
 
     DBUG("\tmaximum_iterations: %d.", maximum_iterations);
 
     auto const error_tolerance =
-        //! \ogs_file_param{material__solid__constitutive_relation__Ehlers__nonlinear_solver__error_tolerance}
+        //! \ogs_file_param{material__solid__constitutive_relation__ThermoPlasticBDT__nonlinear_solver__error_tolerance}
         config.getConfigParameter<double>("error_tolerance");
 
     DBUG("\terror_tolerance: %g.", error_tolerance);
@@ -41,24 +42,24 @@ inline std::unique_ptr<DamagePropertiesParameters> createDamageProperties(
     std::vector<std::unique_ptr<ProcessLib::ParameterBase>> const& parameters,
     BaseLib::ConfigTree const& config)
 {
-    //! \ogs_file_param_special{material__solid__constitutive_relation__Ehlers__damage_properties__alpha_d}
+    //! \ogs_file_param_special{material__solid__constitutive_relation__ThermoPlasticBDT__damage_properties__alpha_d}
     auto& alpha_d =
         ProcessLib::findParameter<double>(config, "alpha_d", parameters, 1);
 
     DBUG("Use \'%s\' as alpha_d.", alpha_d.name.c_str());
 
-    //! \ogs_file_param_special{material__solid__constitutive_relation__Ehlers__damage_properties__beta_d}
+    //! \ogs_file_param_special{material__solid__constitutive_relation__ThermoPlasticBDT__damage_properties__beta_d}
     auto& beta_d =
         ProcessLib::findParameter<double>(config, "beta_d", parameters, 1);
 
     DBUG("Use \'%s\' as beta_d.", beta_d.name.c_str());
 
-    //! \ogs_file_param_special{material__solid__constitutive_relation__Ehlers__damage_properties__h_d}
+    //! \ogs_file_param_special{material__solid__constitutive_relation__ThermoPlasticBDT__damage_properties__h_d}
     auto& h_d = ProcessLib::findParameter<double>(config, "h_d", parameters, 1);
 
     DBUG("Use \'%s\' as h_d.", h_d.name.c_str());
 
-    //! \ogs_file_param{material__solid__constitutive_relation__Ehlers__damage_properties__m_d}
+    //! \ogs_file_param{material__solid__constitutive_relation__ThermoPlasticBDT__damage_properties__m_d}
     double const m_d = config.getConfigParameter<double>("m_d");
     DBUG("Over-non-local value m_d: %g", m_d);
 
@@ -67,137 +68,85 @@ inline std::unique_ptr<DamagePropertiesParameters> createDamageProperties(
 }
 
 template <int DisplacementDim>
-std::unique_ptr<SolidEhlers<DisplacementDim>> createEhlers(
+std::unique_ptr<SolidThermoPlasticBDT<DisplacementDim>> createThermoPlasticBDT(
     std::vector<std::unique_ptr<ProcessLib::ParameterBase>> const& parameters,
     BaseLib::ConfigTree const& config)
 {
     //! \ogs_file_param{material__solid__constitutive_relation__type}
-    config.checkConfigParameter("type", "Ehlers");
-    DBUG("Create Ehlers material");
+    config.checkConfigParameter("type", "ThermoPlasticBDT");
+    DBUG("Create ThermoPlasticBDT material");
 
-    //! \ogs_file_param_special{material__solid__constitutive_relation__Ehlers__shear_modulus}
+    //! \ogs_file_param_special{material__solid__constitutive_relation__ThermoPlasticBDT__shear_modulus}
     auto& shear_modulus = ProcessLib::findParameter<double>(
         config, "shear_modulus", parameters, 1);
 
     DBUG("Use \'%s\' as shear modulus parameter.", shear_modulus.name.c_str());
 
-    //! \ogs_file_param_special{material__solid__constitutive_relation__Ehlers__bulk_modulus}
+    //! \ogs_file_param_special{material__solid__constitutive_relation__ThermoPlasticBDT__bulk_modulus}
     auto& bulk_modulus = ProcessLib::findParameter<double>(
         config, "bulk_modulus", parameters, 1);
 
     DBUG("Use \'%s\' as bulk modulus parameter.", bulk_modulus.name.c_str());
 
-    //! \ogs_file_param_special{material__solid__constitutive_relation__Ehlers__kappa}
-    auto& kappa =
-        ProcessLib::findParameter<double>(config, "kappa", parameters, 1);
+    //! \ogs_file_param_special{material__solid__constitutive_relation__ThermoPlasticBDT__fc}
+    auto& fc = ProcessLib::findParameter<double>(config, "fc", parameters, 1);
 
-    DBUG("Use \'%s\' as kappa.", kappa.name.c_str());
+    DBUG("Use \'%s\' as fc.", fc.name.c_str());
 
-    //! \ogs_file_param_special{material__solid__constitutive_relation__Ehlers__beta}
-    auto& beta =
-        ProcessLib::findParameter<double>(config, "beta", parameters, 1);
+    //! \ogs_file_param_special{material__solid__constitutive_relation__ThermoPlasticBDT__m}
+    auto& m = ProcessLib::findParameter<double>(config, "m", parameters, 1);
 
-    DBUG("Use \'%s\' as beta.", beta.name.c_str());
+    DBUG("Use \'%s\' as m.", m.name.c_str());
 
-    //! \ogs_file_param_special{material__solid__constitutive_relation__Ehlers__gamma}
-    auto& gamma =
-        ProcessLib::findParameter<double>(config, "gamma", parameters, 1);
+    //! \ogs_file_param_special{material__solid__constitutive_relation__ThermoPlasticBDT__qp0}
+    auto& qp0 = ProcessLib::findParameter<double>(config, "qp0", parameters, 1);
 
-    DBUG("Use \'%s\' as gamma.", gamma.name.c_str());
+    DBUG("Use \'%s\' as qp0.", qp0.name.c_str());
 
-    //! \ogs_file_param_special{material__solid__constitutive_relation__Ehlers__hardening_modulus}
-    auto& hardening_modulus = ProcessLib::findParameter<double>(
-        config, "hardening_modulus", parameters, 1);
-
-    DBUG("Use \'%s\' as hardening modulus parameter.",
-         hardening_modulus.name.c_str());
-
-    //! \ogs_file_param_special{material__solid__constitutive_relation__Ehlers__alpha}
+    //! \ogs_file_param_special{material__solid__constitutive_relation__ThermoPlasticBDT__alpha}
     auto& alpha =
         ProcessLib::findParameter<double>(config, "alpha", parameters, 1);
 
     DBUG("Use \'%s\' as alpha.", alpha.name.c_str());
 
-    //! \ogs_file_param_special{material__solid__constitutive_relation__Ehlers__delta}
-    auto& delta =
-        ProcessLib::findParameter<double>(config, "delta", parameters, 1);
+    //! \ogs_file_param_special{material__solid__constitutive_relation__ThermoPlasticBDT__n}
+    auto& n = ProcessLib::findParameter<double>(config, "n", parameters, 1);
 
-    DBUG("Use \'%s\' as delta.", delta.name.c_str());
+    DBUG("Use \'%s\' as n.", n.name.c_str());
 
-    //! \ogs_file_param_special{material__solid__constitutive_relation__Ehlers__eps}
-    auto& eps = ProcessLib::findParameter<double>(config, "eps", parameters, 1);
+    //! \ogs_file_param_special{material__solid__constitutive_relation__ThermoPlasticBDT__T0}
+    auto& T0 = ProcessLib::findParameter<double>(config, "T0", parameters, 1);
 
-    DBUG("Use \'%s\' as eps.", eps.name.c_str());
+    DBUG("Use \'%s\' as T0.", T0.name.c_str());
 
-    //! \ogs_file_param_special{material__solid__constitutive_relation__Ehlers__m}
-    auto& m = ProcessLib::findParameter<double>(config, "m", parameters, 1);
-
-    DBUG("Use \'%s\' as m.", m.name.c_str());
-
-    //! \ogs_file_param_special{material__solid__constitutive_relation__Ehlers__alphap}
-    auto& alphap =
-        ProcessLib::findParameter<double>(config, "alphap", parameters, 1);
-
-    DBUG("Use \'%s\' as alphap.", alphap.name.c_str());
-
-    //! \ogs_file_param_special{material__solid__constitutive_relation__Ehlers__deltap}
-    auto& deltap =
-        ProcessLib::findParameter<double>(config, "deltap", parameters, 1);
-
-    DBUG("Use \'%s\' as deltap.", deltap.name.c_str());
-
-    //! \ogs_file_param_special{material__solid__constitutive_relation__Ehlers__epsp}
-    auto& epsp =
-        ProcessLib::findParameter<double>(config, "epsp", parameters, 1);
-
-    DBUG("Use \'%s\' as epsp.", epsp.name.c_str());
-
-    //! \ogs_file_param_special{material__solid__constitutive_relation__Ehlers__mp}
-    auto& paremeter_mp =
-        ProcessLib::findParameter<double>(config, "mp", parameters, 1);
-
-    DBUG("Use \'%s\' as mp.", paremeter_mp.name.c_str());
-
-    //! \ogs_file_param_special{material__solid__constitutive_relation__Ehlers__betap}
-    auto& betap =
-        ProcessLib::findParameter<double>(config, "betap", parameters, 1);
-
-    DBUG("Use \'%s\' as betap.", betap.name.c_str());
-
-    //! \ogs_file_param_special{material__solid__constitutive_relation__Ehlers__gammap}
-    auto& gammap =
-        ProcessLib::findParameter<double>(config, "gammap", parameters, 1);
-
-    DBUG("Use \'%s\' as gammap.", gammap.name.c_str());
-
-    MaterialPropertiesParameters mp{
-        shear_modulus, bulk_modulus, alpha,  beta,
-        gamma,         delta,        eps,    m,
-        alphap,        betap,        gammap, deltap,
-        epsp,          paremeter_mp, kappa,  hardening_modulus};
+    MaterialPropertiesParameters mp{shear_modulus, bulk_modulus, fc, m,
+                                    qp0,           alpha,        n,  T0};
 
     // Damage properties.
-    std::unique_ptr<DamagePropertiesParameters> ehlers_damage_properties;
+    std::unique_ptr<DamagePropertiesParameters>
+        thermoplasticBDT_damage_properties;
 
-    auto const& ehlers_damage_config =
-        //! \ogs_file_param{material__solid__constitutive_relation__Ehlers__damage_properties}
+    auto const& thermoplasticBDT_damage_config =
+        //! \ogs_file_param{material__solid__constitutive_relation__ThermoPlasticBDT__damage_properties}
         config.getConfigSubtreeOptional("damage_properties");
-    if (ehlers_damage_config)
+    if (thermoplasticBDT_damage_config)
     {
-        ehlers_damage_properties =
-            createDamageProperties(parameters, *ehlers_damage_config);
+        thermoplasticBDT_damage_properties =
+            createDamageProperties(parameters, *thermoplasticBDT_damage_config);
     }
 
     auto const& nonlinear_solver_config =
-        //! \ogs_file_param{material__solid__constitutive_relation__Ehlers__nonlinear_solver}
+        //! \ogs_file_param{material__solid__constitutive_relation__ThermoPlasticBDT__nonlinear_solver}
         config.getConfigSubtree("nonlinear_solver");
     auto const nonlinear_solver_parameters =
         createNewtonRaphsonSolverParameters(nonlinear_solver_config);
 
-    return std::make_unique<SolidEhlers<DisplacementDim>>(
-        nonlinear_solver_parameters, mp, std::move(ehlers_damage_properties));
+    return std::make_unique<SolidThermoPlasticBDT<DisplacementDim>>(
+        nonlinear_solver_parameters,
+        mp,
+        std::move(thermoplasticBDT_damage_properties));
 }
 
-}  // namespace Ehlers
+}  // namespace ThermoPlasticBDT
 }  // namespace Solids
 }  // namespace MaterialLib
