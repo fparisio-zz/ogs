renchao-lu/ogs6

prevent exception in case of Tnp = 0 when using Cohesive zone modelI.

MaterialLib/FractureModels/CohesiveZoneModeI.h
@@ -77,6 +77,11 @@ public:
         double fracture_opening_at_residual_traction(double const t,
                                                      X const& x) const
         {
+            if (_peak_normal_traction(t, x)[0] == 0.0)
+            {
+                return 0.0;
+            }
+
             return 2 * _fracture_toughness(t, x)[0] /
                    _peak_normal_traction(t, x)[0];
         }

fparisio-zz/ogs

MaterialLib/FractureModels/CohesiveZoneModeI.h
@@ -49,6 +49,11 @@ struct MaterialPropertiesParameters
     double fracture_opening_at_residual_traction(double const t,
                                                  X const& x) const
     {
+        if (peak_normal_traction(t, x)[0] == 0.0)
+        {
+            return 0.0;
+        }
+
         return 2 * fracture_toughness(t, x)[0] / peak_normal_traction(t, x)[0];
     }
 